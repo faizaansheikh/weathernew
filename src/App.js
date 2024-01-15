dfe5c5eba3e6e1a2abd87d1b8c3aaaeb7c9d3e6b
Faizaan_sheikh <princefaizan800@gmail.com>
2024-01-15 15:35:17 +0500

    v

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import WetherApp from './Components/WetherApp';
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:'#1b1821',margin:'20px',borderRadius:'30px',padding:'10px'}}>
    <WetherApp />
     </div>
   );
